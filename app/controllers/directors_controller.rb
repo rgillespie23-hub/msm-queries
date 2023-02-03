7945fb2bd4d8c8fc1f9f84ca1f2d3984b6ff1891
rgillespie23 <rgillespie@uchicago.edu>
2023-02-03 05:13:49 +0000

    End of Video

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,4 +5,18 @@ class DirectorsController < ApplicationController
     render({:template => "director_templates/index.html.erb"})
   end
 
+  def wisest
+    @oldest = Director.where.not({ :dob => nil}).order({ :dob => :asc }).at(0)
+
+    render({:template => "director_templates/eldest.html.erb"})
+  end
+
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id }).at(0)
+    @filmography = Movie.where({ :director_id => the_id })
+
+    render({:template => "director_templates/show.html.erb"})    
+  end
+
 end
